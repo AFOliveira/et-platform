AFOliveira/et-platform

commit a9fbdd0e15e8e4ba66b3b10ead3d119d76208631

[sw-sysemu] Fix write_mtimecmp() in rvtimer for sysemu.Update changelog.md

sw-sysemu/devices/rvtimer.h
@@ -51,7 +51,7 @@ struct RVTimer
     void write_mtimecmp(const Agent& agent, uint64_t val)
     {
         bool had_interrupt = interrupt;
-        mtimecmp = val;
+        mtimecmp = mtime+val;
         interrupt = (mtime >= mtimecmp);
         if (had_interrupt && !interrupt) {
             for (unsigned shire = 0; shire < EMU_NUM_SHIRES; ++shire) {

commit 4b9f6517db333736df012bdab713b63afd2875f3

[sw-sysemu] Fix rvtimer behavior
Commit https://github.com/aifoundry-org/et-platform/commit/a9fbdd0e15e8e4ba66b3b10ead3d119d76208631
introduced a bug (reason lost to history).

Correct behavior: when writing to `mtimecmp` register
we should overwrite set value (raise interrupt at time `val`).

Buggy version adds to the current timer value (raise interrupt
*after* `val` duration).

sw-sysemu/devices/rvtimer.h
@@ -46,7 +46,7 @@ struct RVTimer
     void write_mtimecmp(const Agent& agent, uint64_t val)
     {
         bool had_interrupt = interrupt;
-        mtimecmp = mtime+val;
+        mtimecmp = val;
         interrupt = (mtime >= mtimecmp);
         if (had_interrupt && !interrupt) {
             for (unsigned shire = 0; shire < EMU_NUM_SHIRES; ++shire) {