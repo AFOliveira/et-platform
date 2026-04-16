1d1fa5e2279a56e855f3dc651de15ec2f7546452
Afonso Oliveira <afonso.oliveira707@gmail.com>
2026-04-16 16:47:07 +0100

    [hal/platform/erbium] add register-access primitives and ESR backend
    Header-only primitives plus the Erbium ESR address encoder.
    
      include/erbium_hal/mmio.h  reg_read32/write32/modify32,
                                 reg_read64/write64, reg_wait_for_bits,
                                 reg_wait_bit_set/clear
      include/erbium_hal/csr.h   csr_read/write/set/clear (inline asm)
      include/erbium_hal/esr.h   esr_read_u64/write_u64 over a logical
                                 (pp, shire, subregion, offset) tuple;
                                 resolves <esr_platform.h> by -I path
    
      hal/platform/erbium/esr_platform.h  Erbium encoding
                                          base 0x80000000, PP [23:22],
                                          shire [30:24]

---
+++ hal/platform/erbium/include/erbium_hal/csr.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2026 AIFoundry
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * RISC-V CSR access primitives.
+ */
+
+#ifndef ERBIUM_HAL_CSR_H_
+#define ERBIUM_HAL_CSR_H_
+
+#define STRINGIFY_(x) #x
+#define STRINGIFY(x)  STRINGIFY_(x)
+
+#define csr_read(csr)                                         \
+({                                                            \
+	register unsigned long __rv;                          \
+	__asm__ volatile ("csrr %0, " STRINGIFY(csr)          \
+			  : "=r" (__rv));                     \
+	__rv;                                                 \
+})
+
+#define csr_write(csr, val)                                   \
+	do {                                                  \
+		unsigned long __wv = (unsigned long)(val);    \
+		__asm__ volatile ("csrw " STRINGIFY(csr) ", %0" \
+				  :                           \
+				  : "rK" (__wv)               \
+				  : "memory");                \
+	} while (0)
+
+#define csr_set(csr, val)                                     \
+	do {                                                  \
+		unsigned long __sv = (unsigned long)(val);    \
+		__asm__ volatile ("csrs " STRINGIFY(csr) ", %0" \
+				  :                           \
+				  : "rK" (__sv)               \
+				  : "memory");                \
+	} while (0)
+
+#define csr_clear(csr, val)                                   \
+	do {                                                  \
+		unsigned long __cv = (unsigned long)(val);    \
+		__asm__ volatile ("csrc " STRINGIFY(csr) ", %0" \
+				  :                           \
+				  : "rK" (__cv)               \
+				  : "memory");                \
+	} while (0)
+
+#endif /* ERBIUM_HAL_CSR_H_ */
